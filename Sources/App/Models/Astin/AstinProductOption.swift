3b3dbec1c75c5b4cf6b0310faabe1f192f6f9230
Hadi Sharghi <hsharghi@icloud.com>
2019-12-14 19:36:49 +0330

    add optional properties

--- Sources/App/Models/Astin/AstinProductOption.swift
+++ Sources/App/Models/Astin/AstinProductOption.swift
@@ -12,13 +12,19 @@ import FluentSQLite
 final class AstinProductOption: SQLiteModel {
     static var entity: String = "product_options"
 
+    static var sqlTableIdentifierString: String {
+        return self.entity
+    }
+
+
     var id: Int?
     var productId: AstinProduct.ID
     var optionId: Int
     var optionLabel: String
     var optionValue: String
     
-    internal init(productId: AstinProduct.ID, optionId: Int, optionLabel: String, optionValue: String) {
+    internal init(id: Int? = nil, productId: AstinProduct.ID, optionId: Int, optionLabel: String, optionValue: String) {
+        self.id = id
         self.productId = productId
         self.optionId = optionId
         self.optionLabel = optionLabel
@@ -27,3 +33,26 @@ final class AstinProductOption: SQLiteModel {
     
 }
 
+extension AstinProductOption {
+    // this struct should have the same fields as `TodoCategory`, but all optional
+    struct OptionalFields: Decodable {
+        let id: Int?
+        let productId: AstinProduct.ID?
+        let optionId: Int?
+        let optionLabel: String?
+        let optionValue: String?
+    }
+    
+    // add a convenience initializer to make it easy to transform it to the original `TodoCategory` model
+    // note: initializer is failable, as we want to make sure that all mandatory values of `TodoCategory` are provided
+    convenience init?(_ optionalFields: OptionalFields) {
+        guard let productId = optionalFields.productId,
+            let optionId = optionalFields.optionId,
+            let optionLabel = optionalFields.optionLabel,
+            let optionValue = optionalFields.optionValue
+            else {
+                return nil
+        }
+        self.init(id: productId, productId: productId, optionId: optionId, optionLabel: optionLabel, optionValue: optionValue)
+    }
+}
