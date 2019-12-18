4bad6c886e7d698766e1e5ccdffb7fc700145e64
Hadi Sharghi <hsharghi@icloud.com>
2019-12-18 16:51:04 +0330

    all imports

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -3,4 +3,16 @@ import Vapor
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
     // Your code here
+    
+    
+//    let categoryImporter = try CategoryImporter(container: app)
+//    categoryImporter.start()
+//    
+//    let brandImporter = try BrandImporter(container: app)
+//    brandImporter.start()
+//
+    let productImporter = try ProductImporter(container: app)
+    productImporter.start()
+    
+    
 }
